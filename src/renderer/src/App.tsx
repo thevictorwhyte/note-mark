bd6c16912cff44c07d3a4aec173a3cd5ff8d2564
Victor Whyte <victordavidwhyte@gmail.com>
2024-02-07 12:17:14 +0100

    feat: action buttons

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,13 @@
-import { Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
+import { ActionButtonsRow, Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
 
 const App = () => {
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
-        <Sidebar className="p-2">Sidebar</Sidebar>
+        <Sidebar className="p-2">
+          <ActionButtonsRow className="flex justify-between mt-1" />
+        </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
     </>
--- src/renderer/src/components/index.ts
+++ src/renderer/src/components/index.ts
@@ -1,2 +1,4 @@
+export * from './ActionButtonsRow'
 export * from './AppLayout'
+export * from './Button'
 export * from './DraggableTopbar'
